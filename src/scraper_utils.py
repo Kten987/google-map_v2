bf1ee320170254952fe5e25b4c6e5c150e0ed969
Kten987 <thinhlevan060@gmail.com>
2023-12-28 14:57:48 +0700

    update document

--- src/scraper_utils.py
+++ src/scraper_utils.py
@@ -3,21 +3,16 @@ from botasaurus import AntiDetectDriver
 
 def perform_visit(driver: AntiDetectDriver, link:str):
     def visit_gmap_with_consent():
-                    driver.organic_get(link, accept_cookies=True)
-                    
-                    # if driver.is_in_page("https://consent.google.com/"):
-                    #     agree_button_selector = 'form:nth-child(2) > div > div > button'
-                    #     driver.click(agree_button_selector)
-                        # driver.organic_get(link)
+        """
+        Truy cập vào trang Google Maps với sự đồng ý của người dùng.
 
+        Hàm này sử dụng trình duyệt để truy cập vào liên kết được cung cấp và chấp nhận các cookie.
+        Nếu trang hiển thị yêu cầu đồng ý của người dùng, hàm sẽ tự động nhấp vào nút đồng ý.
+        Sau đó, hàm sẽ tiếp tục truy cập vào liên kết ban đầu.
 
-                    # if not driver.is_in_page('maps/search/', bt.Wait.LONG):
-                    #     if driver.is_in_page("consent.google.com", bt.Wait.SHORT):
-                    #         el = driver.get_element_or_none_by_selector(
-                    #             'form:nth-child(2) > div > div > button', bt.Wait.LONG)
-                    #         el.click()
-                    #     visit_gmap_with_consent()
-
+        """
+        driver.organic_get(link, accept_cookies=True)
+        
     def visit_gmap_simple():
                     driver.get_by_current_page_referrer(link)
      
@@ -68,4 +63,16 @@ def create_search_link(query: str, lang, geo_coordinates: str, zoom):
     url += f'?{urllib.parse.urlencode(params)}'
 
     return url
+
+'''Hàm create_search_link được sử dụng để tạo ra một liên kết tìm kiếm trên Google Maps dựa trên các thông tin đầu vào như query (câu truy vấn), lang (ngôn ngữ), geo_coordinates (tọa độ địa lý), và zoom (mức độ phóng to).
+
+Đầu tiên, hàm kiểm tra xem có sự kết hợp không hợp lệ giữa geo_coordinates và zoom hay không. Nếu geo_coordinates là None (không có tọa độ địa lý được cung cấp) và zoom không phải là None (có mức độ phóng to được cung cấp), hàm sẽ raise một ValueError với thông báo "geo_coordinates must be provided along with zoom".
+
+Tiếp theo, hàm sử dụng urllib.parse.quote_plus để mã hóa URL của query (câu truy vấn) để đảm bảo nó được truyền đi một cách an toàn trong URL.
+
+Sau đó, hàm xây dựng các tham số cơ bản cho URL, bao gồm 'authuser', 'hl' (ngôn ngữ), và 'entry'. Nếu lang không phải là None, các tham số này sẽ được thiết lập, ngược lại, chỉ có 'authuser' và 'entry' được thiết lập.
+
+Tiếp theo, hàm xây dựng chuỗi tọa độ địa lý (geo_str) dựa trên geo_coordinates và zoom. Nếu geo_coordinates không phải là None, hàm sẽ kiểm tra xem zoom có khác None hay không. Nếu có, geo_str sẽ được xây dựng với định dạng '/@{remove_spaces(geo_coordinates)},{zoom}z', ngược lại, geo_str sẽ được xây dựng với định dạng '/@{remove_spaces(geo_coordinates)}'.
+
+Cuối cùng, hàm xây dựng URL cuối cùng bằng cách kết hợp endpoint (URL đã được mã hóa), geo_str (nếu có), và các tham số cơ bản. URL cuối cùng được trả về từ hàm.'''
 
--- src/social_scraper.py
+++ src/social_scraper.py
@@ -8,11 +8,37 @@ FAILED_DUE_TO_NOT_SUBSCRIBED = "FAILED_DUE_TO_NOT_SUBSCRIBED"
 FAILED_DUE_TO_UNKNOWN_ERROR = "FAILED_DUE_TO_UNKNOWN_ERROR"
 
 def update_credits():
-    credits_used  = bt.LocalStorage.get_item("credits_used", 0)
+    """
+    Update the credits used by incrementing the value stored in the local storage.
+
+    This function retrieves the current value of 'credits_used' from the local storage,
+    increments it by 1, and then updates the value in the local storage.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    credits_used = bt.LocalStorage.get_item("credits_used", 0)
     bt.LocalStorage.set_item("credits_used", credits_used + 1)
 
 def do_request(data, retry_count=3):
-    
+    """
+    Perform a request to scrape social details from a website.
+
+    This function takes in the data containing the website URL, place ID, and API key.
+    It sends a request to the website-contacts-scraper API to scrape the social details.
+    If the request is successful, it updates the credits used and returns the scraped data.
+    If the request fails due to credits exhaustion, rate limit, or other errors, it returns an error message.
+
+    Parameters:
+        data (dict): The data containing the website URL, place ID, and API key.
+        retry_count (int): The number of retries allowed for the request. Default is 3.
+
+    Returns:
+        dict: The scraped social details or an error message.
+    """
     place_id = data["place_id"]
     website = data["website"]
     key = data["key"]
@@ -28,7 +54,6 @@ def do_request(data, retry_count=3):
         "X-RapidAPI-Host": "website-contacts-scraper.p.rapidapi.com"
     }
 
-    
     response = requests.get(url, headers=headers, params=querystring)
     response_data = response.json()
     if response.status_code == 200:
@@ -41,67 +66,115 @@ def do_request(data, retry_count=3):
 
         final["phones"] = final["phone_numbers"]
         
-
         del final["phone_numbers"]
         del final["domain"]
         del final["query"]
 
-        
         return {
             "place_id": place_id,
             "data": final,
             "error": None
         }
-    # elif response.status_code == 429:
     else: 
         message = response_data.get("message", "")
         if "exceeded the MONTHLY quota" in message:
-            # print("Cddd")
             return  DontCache({
-                        "place_id": place_id,
-                        "data":  None,
-                        "error":FAILED_DUE_TO_CREDITS_EXHAUSTED
-                    })
+                "place_id": place_id,
+                "data":  None,
+                "error":FAILED_DUE_TO_CREDITS_EXHAUSTED
+            })
         elif "exceeded the rate limit per second for your plan" in message or "many requests" in message:
             sleep(2)
             return do_request(data, retry_count - 1)
         elif "You are not subscribed to this API." in message:
-            
             return DontCache({
-                        "place_id": place_id,
-                        "data": None,
-                        "error": FAILED_DUE_TO_NOT_SUBSCRIBED
-                    })
+                "place_id": place_id,
+                "data": None,
+                "error": FAILED_DUE_TO_NOT_SUBSCRIBED
+            })
 
         print(f"Error: {response.status_code}", response_data)
         return  DontCache({
-                        "place_id": place_id,
-                        "data":  None,
-                        "error":FAILED_DUE_TO_UNKNOWN_ERROR
-                    })
-
+            "place_id": place_id,
+            "data":  None,
+            "error":FAILED_DUE_TO_UNKNOWN_ERROR
+        })
 
 @rq(
     close_on_crash=True,
     output=None,
-    )
+)
 def perform_scrape_social(reqs, data):
+    """
+    Perform the scraping of social details using the 'do_request' function.
+
+    This function is decorated with the 'rq' decorator from the 'botasaurus' library.
+    It takes in the 'reqs' and 'data' parameters and calls the 'do_request' function to perform the scraping.
+    The result is returned without any output.
+
+    Parameters:
+        reqs: The request object.
+        data: The data containing the website URL, place ID, and API key.
+
+    Returns:
+        None
+    """
     return do_request(data)
 
 @rq(
     close_on_crash=True,
     output=None,
-    )
+)
 def perform_scrape_social_pro(reqs, data):
+    """
+    Perform the scraping of social details using the 'do_request' function.
+
+    This function is decorated with the 'rq' decorator from the 'botasaurus' library.
+    It takes in the 'reqs' and 'data' parameters and calls the 'do_request' function to perform the scraping.
+    The result is returned without any output.
+
+    Parameters:
+        reqs: The request object.
+        data: The data containing the website URL, place ID, and API key.
+
+    Returns:
+        None
+    """
     return do_request(data)
 
 def is_free():
+    """
+    Check if the credits used is within the free limit.
+
+    This function checks the value of 'credits_used' from the local storage.
+    If the value is less than the free credits plus 10, it returns True.
+    Otherwise, it returns False.
+
+    Parameters:
+        None
+
+    Returns:
+        bool: True if within the free limit, False otherwise.
+    """
     FREE_CREDITS_PLUS_10 = 60
-    # Assuming bt.LocalStorage is used to get the credits_used value
     credits_used = bt.LocalStorage.get_item("credits_used", 0)
     return credits_used < FREE_CREDITS_PLUS_10
 
 def scrape_social(social_data, cache):
+    """
+    Scrape social details based on the provided data.
+
+    This function takes in the social data and cache parameters.
+    It checks if the scraping should be performed using the free or pro version.
+    It then calls the respective 'perform_scrape_social' function to perform the scraping.
+
+    Parameters:
+        social_data (dict): The data containing the website URL, place ID, and API key.
+        cache: The cache object.
+
+    Returns:
+        The result of the scraping.
+    """
     free = is_free()
     if free:
         return perform_scrape_social(social_data, cache=cache)
